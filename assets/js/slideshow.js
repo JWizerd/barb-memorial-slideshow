dcffef1b52ed46b02f7dc5fe39260c8b932a245f
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2022-01-06 16:20:37 -0700

    created custom autoplay feature to fix dynamic duration issue with audio, and to extended last slide duration

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -2,50 +2,40 @@ const IMG_COUNT = 51;
 const audio = document.getElementById("audio");
 const headline = document.getElementById('headline');
 const initialHeadlineText = headline.innerText;
+const SLIDE_DURATION = Math.floor(((audio.duration / IMG_COUNT) * 1000));
 const finalHeadlineText = "Home at last with Stan..."
-const SLIDE_DURATION = (audio.duration / IMG_COUNT) * 1000;
 const sliderOptions = {
   container: "#slider",
   items: 1,
   slideBy: 1,
   speed: 300,
-  autoplay: true,
   loop: false,
-  autoplayTimeout: SLIDE_DURATION,
+  nav: false,
   swipeAngle: false,
   controlsPosition: 'bottom',
-  nav: false,
-  arrowKeys: true,
   controlsText: [
     '<i class="fa fa-chevron-left"></i>',
     '<i class="fa fa-chevron-right"></i>'
   ]
 }
 
-async function appendImages(imgCount = IMG_COUNT) {
-  return new Promise((resolve, reject) => {
-    try {
-      const slider = document.createElement('div');
-      slider.id = 'slider';
+function appendImages(imgCount = IMG_COUNT) {
+  const slider = document.createElement('div');
+  slider.id = 'slider';
 
-      for (let count = 1; count <= imgCount; count++) {
-        const slideItem = document.createElement('div');
-        slideItem.className = 'item';
+  for (let count = 1; count <= imgCount; count++) {
+    const slideItem = document.createElement('div');
+    slideItem.className = 'item';
 
-        const img = document.createElement('img');
-        img.className = 'slide';
-        img.src = 'assets/img/slides/' + count + '.jpg';
-        slideItem.appendChild(img);
+    const img = document.createElement('img');
+    img.className = 'slide';
+    img.src = 'assets/img/slides/' + count + '.jpg';
+    slideItem.appendChild(img);
 
-        slider.appendChild(slideItem);
-      }
+    slider.appendChild(slideItem);
+  }
 
-      document.getElementById('slider-wrapper').appendChild(slider);
-      resolve();
-    } catch (error) {
-      reject(error);
-    }
-  });
+  document.getElementById('slider-wrapper').appendChild(slider);
 }
 
 function showArrowBtns() {
@@ -59,8 +49,20 @@ function hideArrowBtns() {
 }
 
 function playSlideshow(slider) {
-  slider.play();
+  const audio = document.getElementById("audio");
+  const DURATION = Math.floor(((audio.duration / IMG_COUNT) * 1000));
   audio.play();
+
+  let idx = 1;
+  const autoplay = setInterval(() => {
+    slider.goTo(idx);
+    idx++;
+
+    // this check allows the last image to run for 2x DURATION
+    // which allows for the final text to display longer
+    if (idx === IMG_COUNT) clearInterval(autoplay)
+  }, DURATION);
+
   hidePlayButton();
   hideArrowBtns();
 }
@@ -73,28 +75,19 @@ function hidePlayButton() {
   document.getElementById('slideshow').classList.add('hide');
 }
 
-async function init(){
-  try {
-    await appendImages();
-    const slider = tns(sliderOptions);
+function init(){
+  appendImages();
+  const slider = tns(sliderOptions);
+  document.getElementById('slideshow').addEventListener('click', () => playSlideshow(slider));
+  document.getElementById('restart').addEventListener('click', restart);
+  document.querySelectorAll('.tns-controls button').forEach(btn => btn.addEventListener('click', hidePlayButton));
 
-    // pause slideshow at first to let user decide which action to take
-    slider.pause();
+  audio.onended = function() {
+    headline.innerText = finalHeadlineText;
 
-    document.getElementById('slideshow').addEventListener('click', () => playSlideshow(slider));
-    document.getElementById('restart').addEventListener('click', restart);
-    document.querySelectorAll('.tns-controls button').forEach(btn => btn.addEventListener('click', hidePlayButton));
-
-    audio.onended = function() {
-      headline.innerText = finalHeadlineText;
-      slider.pause();
-
-      setTimeout(() => {
-        restart();
-      }, 10 * 1000);
-    }
-  } catch (error) {
-    console.error(error);
+    setTimeout(() => {
+      restart();
+    }, 10 * 1000);
   }
 }
 
